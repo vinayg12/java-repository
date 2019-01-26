7f0870027471308496fd5ffc37c15b8d0e14a53c
Vinay Villers <vinay.ghi1@gmail.com>
2019-01-26 10:33:41 +0000

    Sandeep waste

--- npowern.java
+++ npowern.java
@@ -3,7 +3,7 @@ class Main {
     public static void main(String[] args) {
     	int NthTerm, Number = 2, Counter = 1, Result = 1;
     	Scanner input = new Scanner(System.in);
-      System.out.print("Enter the number upto which you want the series:");
+        System.out.print("Enter the number upto which you want the series:");
     	NthTerm = input.nextInt();
     	System.out.print("The series upto " + NthTerm + " is");
     	for(Result = 1; Result <= NthTerm; )
