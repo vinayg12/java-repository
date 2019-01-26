b5fca831ccb12fcdd777f45a574b22e9920fb5a2
Vinay Villers <vinay.ghi1@gmail.com>
2019-01-26 09:11:34 +0000

    Sandeep waste

---
+++ npowern.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+    	int NthTerm, Number = 2, Counter = 1, Result = 1;
+    	Scanner input = new Scanner(System.in);
+      System.out.print("Enter the number upto which you want the series:");
+    	NthTerm = input.nextInt();
+    	System.out.print("The series upto " + NthTerm + " is");
+    	for(Result = 1; Result <= NthTerm; )
+    	{
+    	  System.out.print(" " + Result);
+    	  Result = 1;
+    	  while(Counter <= Number)
+    	  {
+    	    Result = Result * Number;
+    	    Counter++;
+    	  }
+    	  Number++;
+    	  Counter = 1;
+    	}
+    	System.out.print(".");
+  }
+}
