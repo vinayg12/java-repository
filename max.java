3b1fa9e3fac3275d959dd0f285fe6794ad937311
Vinay Villers <vinay.ghi1@gmail.com>
2019-01-31 07:05:52 +0000

    sandeep is the best

---
+++ max.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+    int[] Number;
+    int Counter, Maximum;
+    Number = new int[10];
+    Scanner input = new Scanner(System.in);
+    System.out.print("To find the maximum of 10 numbers, enter the first number:");
+  	Number[0] = input.nextInt();
+  	for(Counter = 1; Counter <= 9; Counter++)
+  	{
+      System.out.print("Enter the next number:");
+      Number[Counter] = input.nextInt();
+    }
+    Maximum = Number[1];
+    for (Counter = 0; Counter < 10; Counter++){
+      if (Maximum < Number[Counter])
+        Maximum = Number[Counter];
+      }
+      System.out.print( Maximum + " is maximum among 10 numbers.");
+    }  
+    
+}
